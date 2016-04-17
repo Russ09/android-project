e3e7894465d95b7b414e5e2538b0fb83e96335db
Russ Bates <russjbates@gmail.com>
2016-04-17 10:42:41 +0000

    Initial Commit

---
+++ App1/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            TextView input_text = FindViewById<TextView>(Resource.Id.textView1);
+            TextView output_text = FindViewById<TextView>(Resource.Id.textView2);
+
+            button.Click += delegate { output_text.Text = string.Format("Hello {0}", input_text.Text); };
+        }
+    }
+}
+
